1e7ead75c967b74462b39919e708049f632a2623
Michael <michael@toms-mbp.home>
2019-08-22 14:34:03 +0100

    Uhh extra notes
    git multi line commits lol first

--- arrays.lua
+++ arrays.lua
@@ -1,7 +1,6 @@
 --[[
 number = 1
 word = "testing"
-]]
 
 animals = { "cat", "dog", "wolf", "whale", "dolphin", "crow" }
 
@@ -9,3 +8,13 @@ for i, item in ipairs(animals) do
     print(i .. "  " .. item)
 end
 
+]]
+a = {}    -- new array
+for i=0, 1000 do
+	a[i] = i 
+end
+
+for i=0, 1000 do
+	print(a[i])
+
+end
